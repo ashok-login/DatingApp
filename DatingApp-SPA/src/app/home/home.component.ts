4720680bf1d6ec54f944005cb8aa0d47d7e3c516
Ashok Kumar K <kak.mca@hotmail.com>
2020-05-28 10:10:00 +0530

    Udemy course 4th section completed

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,22 +8,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getValues();
-  }
 
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(
-      (response) => {
-        this.values = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 
   registerToggle() {
