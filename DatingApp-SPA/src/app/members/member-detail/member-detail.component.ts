0e2290f37711d40f8faf73223dd3583e9e255e99
Ashok Kumar K <kak.mca@hotmail.com>
2020-07-24 13:45:30 +0530

    learning Azure App Service

---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/User';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+// import { error } from 'protractor';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  constructor(private userService: UserService,
+              private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // this.loadUser();
+    this.route.data.subscribe(data => {
+      const user = 'user';
+      this.user = data[user];
+    });
+  }
+
+  // loadUser() {
+  //   const id = 'id';
+  //   this.userService.getUser(+this.route.snapshot.params[id])
+  //           .subscribe((user: User) => {
+  //             this.user = user;
+  //           }, error => {
+  //             this.alertify.error(error);
+  //           });
+  // }
+}
--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Helpers
 {
@@ -21,8 +22,12 @@ namespace DatingApp.API.Helpers
         {
             var paginationHeader = new PaginationHeader(currentPage,
                             itemsPerPage, totalItems, totalPages);
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver 
+                            = new CamelCasePropertyNamesContractResolver();
             response.Headers.Add("Pagination",
-                        JsonConvert.SerializeObject(paginationHeader));
+                        JsonConvert.SerializeObject(paginationHeader,
+                                                        camelCaseFormatter));
             response.Headers.Add("Access-Control-Expose-Headers",
                                                 "Pagination");
 
