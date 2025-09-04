de999ab18e71d218e48ba36ddc7576ab7b4335e0
Ashok-Kumar-Kottagundu <ashok.kottagundu@gainwelltechnologies.com>
2025-09-04 19:40:25 +0530

    Some more learnings

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
-
 await app.RunAsync();
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private readonly http = inject(HttpClient);
+  protected readonly title = signal('Dating app');
+  protected members = signal<any>([]);
+
+  ngOnInit() {
+    this.getMembers()
+      .then((members) => {
+        this.members.set(members);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
